8e1ef4baafe79a592ac365c2b5d49d3c8962e63f
EstherNyanchoka3919 <esterio18031@gmail.com>
2026-03-25 08:52:30 +0300

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -1,7 +1,1 @@
-CREATE TABLE Student (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    first_name VARCHAR(255),
-    last_name VARCHAR(255),
-    email VARCHAR(255)
-);
--- Additional DDL scripts for other entities would go here
+
